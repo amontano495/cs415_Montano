b14b34ac6634cac7b5307c7f9143a29e5b926dd3
adam <amontano495@gmail.com>
2017-02-20 20:56:59 -0800

    deleted:    CS477/PA1/One_box.sh 	deleted:    CS477/PA1/README.md 	deleted:    CS477/PA1/Timing.sh 	deleted:    CS477/PA1/Two_box.sh 	deleted:    CS477/PA1/buidscript.sh 	deleted:    CS477/PA1/buildscript.sh 	deleted:    CS477/PA1/log/one_box.log 	deleted:    CS477/PA1/log/timing.log 	deleted:    CS477/PA1/log/timing.plt 	deleted:    CS477/PA1/log/two_box.log 	deleted:    CS477/PA1/makefile 	deleted:    CS477/PA1/src/PA01-2.c 	deleted:    CS477/PA1/src/PA01.c 	deleted:    CS477/PA1/timing.png 	deleted:    PA0/README.md 	deleted:    PA0/buildscript.sh 	deleted:    PA0/makefile 	deleted:    PA0/src/mpi_hello.c

--- CS477/PA1/One_box.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH -n 2
-#SBATCH --mem=2048MB
-#SBATCH --ntasks-per-node=2
-#SBATCH --output=../log/one_box.log
-srun -n2 PA01
--- CS477/PA1/Timing.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH -n 2
-#SBATCH --mem=2048MB
-#SBATCH --ntasks-per-node=1
-#SBATCH --output=../log/timing.log
-srun -n2 PA01-2
